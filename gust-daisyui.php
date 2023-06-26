98c929089fbee5f156122a3bc78e5c7c18fee093
Matt Sanders <matt@getgust.com>
2023-06-27 05:52:03 +1200

    Adds WP coding standards

--- gust-daisyui.php
+++ gust-daisyui.php
@@ -11,14 +11,14 @@
  * @package GustDaisyui
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
-define('GUST_DAISYUI_PLUGIN_VERSION', '0.1.5');
+define( 'GUST_DAISYUI_PLUGIN_VERSION', '0.1.5' );
 
-if (! defined('GUST_DAISYUI_PLUGIN_FILE') ) {
-    define('GUST_DAISYUI_PLUGIN_FILE', __FILE__);
+if ( ! defined( 'GUST_DAISYUI_PLUGIN_FILE' ) ) {
+	define( 'GUST_DAISYUI_PLUGIN_FILE', __FILE__ );
 }
 
-if (! class_exists('Gust_Daisyui') ) {
-    include_once dirname(__FILE__) . '/includes/class-gust-daisyui.php';
+if ( ! class_exists( 'Gust_Daisyui' ) ) {
+	include_once dirname( __FILE__ ) . '/includes/class-gust-daisyui.php';
 }
