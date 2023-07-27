8e38da546991bffa534dcc2db30b01f96c2e93d4
Matt Sanders <matt@getgust.com>
2023-07-28 06:45:55 +1200

    Adds select

--- includes/class-gust-daisyui.php
+++ includes/class-gust-daisyui.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Gust Daisy UI
  * 
@@ -12,6 +11,8 @@ defined( 'ABSPATH' ) || exit;
  * Main Gust Daisy UI class.
  */
 class Gust_Daisyui {
+
+
 
 
 
@@ -47,7 +48,7 @@ class Gust_Daisyui {
 	 * Gust Daisyui constructor
 	 */
 	public function __construct() {
-		$this->define_constants();
+		 $this->define_constants();
 		$this->includes();
 		$this->register_hooks();
 	}
@@ -64,14 +65,24 @@ class Gust_Daisyui {
 	/**
 	 * Includes files
 	 */
-	private function includes() {
-	}
+	private function includes() {   }
 
 	/**
 	 * Registers hooks
 	 */
 	private function register_hooks() {
-		add_action( 'gust_register_components', [ $this, 'register_components' ] );
+		 add_action( 'gust_register_components', [ $this, 'register_components' ] );
+		add_filter( 'gust_tag_pre_output', [ $this, 'tag_pre_output' ], 10, 5 );
+	}
+
+	/**
+	 * Determines wether an array has a key or not
+	 *
+	 * @param string $key The key to check for.
+	 * @param mixed  $array The array to check.
+	 */
+	private static function array_has_key( $key, $array ) {
+		return array_key_exists( $key, $array ) && ! empty( $array[ $key ] );
 	}
 
 	/** 
@@ -97,6 +108,7 @@ class Gust_Daisyui {
 				'checkbox',
 				'radio',
 				'range',
+				'select',
 			),
 			'layout'       => array(
 				'artboard',
@@ -116,6 +128,54 @@ class Gust_Daisyui {
 				}
 			}
 		}
+	}
+
+	/**
+	 * Filters the output of various components.
+	 *
+	 * @param string $output The HTML to putout.
+	 * @param mixed  $context Any context that is applied.
+	 * @param object $tag Details of the tag.
+	 * @param mixed  $component_details The component details, if any, otherwise null.
+	 * @param mixed  $parent_component_details The parent component details, if any, otherwise null.
+	 */
+	public function tag_pre_output( $output, $context, $tag, $component_details, $parent_component_details ) {
+		if ( ! self::array_has_key( 'id', $tag ) ) {
+			return $output;
+		}
+		switch ( $tag['id'] ) {
+			case 'daisyui-select-input': 
+				return $this->select_input_output( $parent_component_details );
+
+			default: 
+				return $output;
+
+		}
+	}
+
+	/**
+	 * Outputs HTML options for the select component.
+	 *
+	 * @param mixed $parent_component_details The parent component details, if any, otherwise null.
+	 */
+	private function select_input_output( $parent_component_details ) {
+		$options = '<option selected disabled>Pick one</option>';
+		if ( ! $parent_component_details && ! self::array_has_key( 'options', $parent_component_details ) && ! self::array_has_key( 'selectOptions', $parent_component_details['options'] ) ) {
+			return $options;
+		}
+
+		$option_lines = explode( "\n", $parent_component_details['options']['selectOptions'] );
+		foreach ( $option_lines as $option_line ) {
+			$trimmed_line = trim( $option_line );
+			if ( empty( $trimmed_line ) ) {
+				continue;
+			}
+			$value_labels = explode( '=', $trimmed_line );
+			$value        = $value_labels[0];
+			$label        = self::array_has_key( 1, $value_labels ) ? $value_labels[1] : $value_labels[0];
+			$options     .= '<option value="' . esc_attr( $value ) . '">' . esc_html( $label ) . '</option>';
+		}
+		return $options;
 	}
 }
 Gust_Daisyui::instance();
